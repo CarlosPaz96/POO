d010b9f42ac0e51c2ac77c0271cb2424be9916e2
CarlosPaz96 <38087007+CarlosPaz96@users.noreply.github.com>
2018-04-04 16:07:45 -0600

    tarea 1 poo

---
+++ TareaBasesDatos.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea.bases.datos;
+
+/**
+ *
+ * @author Carlos "El Nigga" Paz
+ */
+public class TareaBasesDatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Nombre:Carlos Ernesto Vasquez Paz \n Carnet:00224317 \n Carrera:Ingenieria Informatica \nFecha de nacimiento:07 Diciembre 1996 Estado civil: Soltero  \nLugar de estudio:Universidad Centroamericana Jose Simeon Cañas \nEdad:21");
+    }
+    
+}
